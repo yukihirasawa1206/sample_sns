6020b63324080dc375cf837080837af088e67ce2
yuki <h_x04yjr0@yahoo.co.jp>
2018-12-22 13:27:01 +0000

    Likeモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :likes, dependent: :destroy
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[twitter]
+         
   validates :name, presence: true, length: {maximum: 50}
   
   def self.from_omniauth(auth)
