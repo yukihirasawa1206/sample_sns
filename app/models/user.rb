f110ebd2581e35c907e7b0cca0d1ea7077cbba77
yuki <h_x04yjr0@yahoo.co.jp>
2018-12-24 01:07:36 +0000

    Comment Model を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
