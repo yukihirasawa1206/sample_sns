f0a887cde60f1819fabbeb1149257400d7dd6812
yuki <h_x04yjr0@yahoo.co.jp>
2018-12-28 05:16:00 +0000

    Bookmark Modelの作成等

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :likes, ->{order(created_at: :desc)}, dependent: :destroy
   has_many :comments, ->{order(created_at: :desc)}, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
   
   def is_belongs_to? user
     Post.find_by(id: id, user_id: user.id)
@@ -12,4 +13,7 @@ class Post < ApplicationRecord
     Like.find_by(user_id: user.id, post_id: id)
   end
   
+  def is_bookmarked user
+    Bookmark.find_by(user_id: user.id, post_id: id)
+  end
 end
