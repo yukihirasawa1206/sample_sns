1dfd97bad34e558874180839cb928d9fd84cccfc
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-01-05 17:14:11 +0900

    validationの追加

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+	#before do
+	#	@user = FactoryBot.create(:user)
+	#	@post = FactoruBot.create(:post)
+	#	@comment = @user.comments.build(content: "test comment",post_id: @post.ic, user_id: @user.id)
+	#end
+
+	it "is valid with content, user_id, post_id"
+
+	it "is invalid without user_id"
+
+	it "is invalid without post_id" 
+
+	it "is invalid without content length more than 50 char" 
+
+	
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+	before do
+		@user = FactoryBot.build(:user)
+	end
   
   it "is valid with name, email, password, password_confirmation" do
-    expect(FactoryBot.build(:user)).to be_valid
+		expect(@user).to be_valid
   end
-  
+
+	it "is invalid user with name" do
+		@user.name = nil
+		@user.valid?
+		expect(@user.errors[:name]).to include("can't be blank")
+	end
+
+	it "is invalid user with email" do
+		@user.email = nil
+		@user.valid?
+		expect(@user.errors[:email]).to include("can't be blank")
+	end
 end
