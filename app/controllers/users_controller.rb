6f358b776f1d1d3e61438a9b640ceecc9f065cd3
yuki <h_x04yjr0@yahoo.co.jp>
2019-01-16 18:01:20 +0000

    フロントエンドの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_user, only:[:show, :following, :followers]
+  before_action :find_user, only:[:show, :destroy, :following, :followers]
   
   def index
     @users = User.all
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    @user = User.find(params[:id])
     if current_user?(@user)
       @user.destroy
     else
